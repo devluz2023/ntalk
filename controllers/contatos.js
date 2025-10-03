e1698a5a6c7c3bdb356d969868f0ca960f8f5881
devluz2023 <fabio.jdluz@gmail.com>
2025-10-03 15:47:14 -0300

    ajustes

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -1,69 +1,68 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
   var Usuario = app.models.usuario;
 
   var ContatoController = {
 
-    index: function(req, res) {
-        res.render('contatos/index', resultado);
-      // var _id = req.session.usuario._id;
-      Usuario.findById(1, function(erro, usuario) {
-        var contatos = usuario.contatos;
-        var resultado = {contatos: contatos};
-        res.render('contatos/index', resultado);
-      });
+    index: function (req, res) {
+      res.render('contatos/index');
     },
 
-    create: function(req, res) {
-      var _id = req.session.usuario._id;
-      Usuario.findById(_id, function(erro, usuario) {
-        var contato = req.body.contato;
-        usuario.contatos.push(contato);
-        usuario.save(function() {
-          res.redirect('/contatos');
-        });
-      });
-    },
+    
+  async create(req, res) {
+    try {
+      const { nome, email } = req.body.contato;
+      if (!nome || !email) {
+        return res.status(400).send('Nome e email são obrigatórios');
+      }
+      const usuario = new Usuario({ nome, email });
+      await usuario.save();
+      res.redirect('/contatos');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao criar usuário');
+    }
+  },
 
-    show: function(req, res) {
+    show: function (req, res) {
       var _id = req.session.usuario._id;
-      Usuario.findById(_id, function(erro, usuario) {
+      Usuario.findById(_id, function (erro, usuario) {
         var contatoID = req.params.id;
         var contato = usuario.contatos.id(contatoID);
-        var resultado = {contato: contato};
+        var resultado = { contato: contato };
         res.render('contatos/show', resultado);
       });
     },
 
-    edit: function(req, res) {
+    edit: function (req, res) {
       var _id = req.session.usuario._id;
-      Usuario.findById(_id, function(erro, usuario) {
+      Usuario.findById(_id, function (erro, usuario) {
         var contatoID = req.params.id;
         var contato = usuario.contatos.id(contatoID);
-        var resultado = {contato: contato};
+        var resultado = { contato: contato };
         res.render('contatos/edit', resultado);
       });
     },
 
-    update: function(req, res) {
+    update: function (req, res) {
       var _id = req.session.usuario._id;
-      Usuario.findById(_id, function(erro, usuario) {
+      Usuario.findById(_id, function (erro, usuario) {
         var contatoID = req.params.id;
         var contato = usuario.contatos.id(contatoID);
         contato.nome = req.body.contato.nome;
         contato.email = req.body.contato.email;
-        usuario.save(function() {
+        usuario.save(function () {
           res.redirect('/contatos');
         });
       });
     },
 
-    destroy: function(req, res) {
+    destroy: function (req, res) {
       var _id = req.session.usuario._id;
-      Usuario.findById(_id, function(erro, usuario) {
+      Usuario.findById(_id, function (erro, usuario) {
         var contatoID = req.params.id;
         usuario.contatos.id(contatoID).remove();
-        usuario.save(function() {
+        usuario.save(function () {
           res.redirect('/contatos');
         });
       });
